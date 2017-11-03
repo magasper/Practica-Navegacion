2346d05d21c46a2ade8835896e6a587ac3e6e084
Miguel Angel Larios Torres <mlariospc@corporativosade.com.mx>
2017-11-03 08:53:11 -0600

    Initial Commit

---
+++ Practica Navegacion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Practica Navegacion
+//
+//  Created by pegasus on 03/11/17.
+//  Copyright © 2017 Pegasus Control. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Practica Navegacion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Practica Navegacion
+//
+//  Created by pegasus on 03/11/17.
+//  Copyright © 2017 Pegasus Control. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
